64530a26b860b812edb5cc92df7d72ff1c201b29
Costas K <11378310+kacos2000@users.noreply.github.com>
2018-05-20 14:10:11 +0300

    Add files via upload
    Either import to your SQLite program, or Copy Paste the code to the query window.
    Your software needs to support the SQLIte JSON1 extension (https://www.sqlite.org/json1.html).
    DB Browser for SQLite (http://sqlitebrowser.org/), and
    SQLiteStudio (https://sqlitestudio.pl/index.rvt) 
    have been tested.

---
+++ Timeline_all.sql
@@ -0,0 +1,70 @@
+-- SQLite query to get any useful results from MS Windows 1803 Timeline feature's database (ActivitiesCache.db).
+-- Dates/Times in the database are stored in Unixepoch and UTC by default. 
+-- Using the 'localtime" in the field converts it to our TimeZone.
+-- 
+-- © Costas Katsavounidis (kacos2000 [at] gmail.com)
+-- May 2018
+
+SELECT ActivityOperation.ETag AS Etag, -- This the ActivityOperation Query
+       json_extract(ActivityOperation.Payload, '$.appDisplayName') AS [Program Name],
+       json_extract(ActivityOperation.AppId, '$[1].application') AS Application,
+       json_extract(ActivityOperation.Payload, '$.displayText') AS [File/title opened],
+       json_extract(ActivityOperation.Payload, '$.description') AS [Full Path /Url],
+       Activity_PackageId.Platform AS Platform_id,
+       ActivityOperation.OperationType AS Status,
+       CASE ActivityOperation.ActivityType WHEN 5 THEN 'Open App/File/Page' WHEN 6 THEN 'App In Use/Focus' ELSE 'Unknown yet' END AS [Activity type],
+       json_extract(ActivityOperation.OriginalPayload, '$.type') AS Type,
+       json_extract(ActivityOperation.OriginalPayload, '$.appDisplayName') AS [Original Program Name],
+       json_extract(ActivityOperation.OriginalPayload, '$.displayText') AS [Original File/title opened],
+       json_extract(ActivityOperation.OriginalPayload, '$.description') AS [Original Full Path /Url],
+       json_extract(ActivityOperation.Payload, '$.activeDurationSeconds') AS [Active Duration Secs],
+       json_extract(ActivityOperation.Payload, '$.activeDurationSeconds') AS [Original Duration Secs],
+       CASE WHEN CAST ( (ActivityOperation.EndTime - ActivityOperation.StartTime) AS INTEGER) < 0 THEN '-' ELSE CAST ( (ActivityOperation.EndTime - ActivityOperation.StartTime) AS INTEGER) END AS [Calculated Duration],
+       datetime(ActivityOperation.StartTime, 'unixepoch', 'localtime') AS StartTime, 
+       datetime(ActivityOperation.LastModifiedTime, 'unixepoch', 'localtime') AS LastModified,
+       CASE WHEN ActivityOperation.OriginalLastModifiedOnClient > 0 THEN datetime(ActivityOperation.OriginalLastModifiedOnClient, 'unixepoch', 'localtime') ELSE '  -  ' END AS LastModifiedOnClient,
+       CASE WHEN ActivityOperation.EndTime > 0 THEN datetime(ActivityOperation.EndTime, 'unixepoch', 'localtime') ELSE "-" END AS EndTime,
+       CASE WHEN ActivityOperation.CreatedInCloud > 0 THEN datetime(ActivityOperation.CreatedInCloud, 'unixepoch', 'localtime') ELSE "-" END AS CreatedInCloud,
+	   json_extract(ActivityOperation.OriginalPayload, '$.userTimezone') AS TZone,
+       CAST ( (ActivityOperation.ExpirationTime - ActivityOperation.LastModifiedTime) AS INTEGER) / '86400' AS [Expires In days],
+       datetime(Activity_PackageId.ExpirationTime, 'unixepoch', 'localtime') AS [Expiration on PackageID],
+       datetime(ActivityOperation.ExpirationTime, 'unixepoch', 'localtime') AS Expiration,
+       '{' || substr(hex(Activity_PackageId.ActivityId), 1, 8) || '-' || substr(hex(Activity_PackageId.ActivityId), 9, 4) || '-' || substr(hex(Activity_PackageId.ActivityId), 13, 4) || '-' || substr(hex(Activity_PackageId.ActivityId), 17, 4) || '-' || substr(hex(Activity_PackageId.ActivityId), 21, 12) || '}' AS [Timeline Entry unique GUID]
+	FROM Activity_PackageId
+	JOIN ActivityOperation ON Activity_PackageId.ActivityId = ActivityOperation.Id
+WHERE Platform_id = json_extract(ActivityOperation.AppId, '$[0].platform') AND Activity_PackageId.ActivityId = ActivityOperation.Id
+
+UNION  -- Join Activity & ActivityOperation Queries to get results from both Tables
+
+SELECT Activity.ETag AS Etag,  -- This the Activity Query
+       json_extract(Activity.Payload, '$.appDisplayName') AS [Program Name],
+       json_extract(Activity.AppId, '$[0].application') AS Application,
+       json_extract(Activity.Payload, '$.displayText') AS [File/title opened],
+       json_extract(Activity.Payload, '$.description') AS [Full Path /Url],
+       Activity_PackageId.Platform AS Platform_id,
+       Activity.ActivityStatus AS Status,
+       CASE Activity.ActivityType WHEN 5 THEN 'Open App/File/Page' WHEN 6 THEN 'App In Use/Focus' ELSE 'Unknown yet' END AS [Activity type],
+       json_extract(Activity.OriginalPayload, '$.type') AS Type,
+       json_extract(Activity.OriginalPayload, '$.appDisplayName') AS [Original Program Name],
+       json_extract(Activity.OriginalPayload, '$.displayText') AS [Original File/title opened],
+       json_extract(Activity.OriginalPayload, '$.description') AS [Original Full Path /Url],
+       json_extract(Activity.Payload, '$.activeDurationSeconds') AS [Active Duration Secs],
+       json_extract(Activity.OriginalPayload, '$.activeDurationSeconds') AS [Original Duration Secs],
+       CASE WHEN CAST ( (Activity.EndTime - Activity.StartTime) AS INTEGER) < 0 THEN '-' ELSE CAST ( (Activity.EndTime - Activity.StartTime) AS INTEGER) END AS [Calculated Duration],
+       datetime(Activity.StartTime, 'unixepoch', 'localtime') AS StartTime,
+       datetime(Activity.LastModifiedTime, 'unixepoch', 'localtime') AS LastModified,
+       CASE WHEN Activity.OriginalLastModifiedOnClient > 0 THEN datetime(Activity.OriginalLastModifiedOnClient, 'unixepoch', 'localtime') ELSE '  -  ' END AS LastModifiedOnClient,
+       CASE WHEN Activity.EndTime > 0 THEN datetime(Activity.EndTime, 'unixepoch', 'localtime') ELSE "-" END AS EndTime,
+       CASE WHEN Activity.CreatedInCloud > 0 THEN datetime(Activity.CreatedInCloud, 'unixepoch', 'localtime') ELSE "-" END AS CreatedInCloud,
+       json_extract(Activity.OriginalPayload, '$.userTimezone') AS TZone,
+       CAST ( (Activity.ExpirationTime - Activity.LastModifiedTime) AS INTEGER) / '86400' AS [Expires In days],
+       datetime(Activity_PackageId.ExpirationTime, 'unixepoch', 'localtime') AS [Expiration on PackageID],
+       datetime(Activity.ExpirationTime, 'unixepoch', 'localtime') AS Expiration,
+       '{' || substr(hex(Activity_PackageId.ActivityId), 1, 8) || '-' || substr(hex(Activity_PackageId.ActivityId), 9, 4) || '-' || substr(hex(Activity_PackageId.ActivityId), 13, 4) || '-' || substr(hex(Activity_PackageId.ActivityId), 17, 4) || '-' || substr(hex(Activity_PackageId.ActivityId), 21, 12) || '}' AS [Timeline Entry unique GUID]
+	FROM Activity_PackageId
+    JOIN        Activity ON Activity_PackageId.ActivityId = Activity.Id  
+	WHERE Platform_id = json_extract(Activity.AppId, '$[0].platform') AND Activity_PackageId.ActivityId = Activity.Id
+ 
+ ORDER BY Etag DESC;  -- Edit this line to change the sorting 
+ 
+ -- EOF
