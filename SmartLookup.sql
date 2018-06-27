22b50bca6b0abfd08efcc74afef7913ea34e18c9
Costas K <11378310+kacos2000@users.noreply.github.com>
2018-06-27 16:31:26 +0300

    Update SmartLookup.sql

--- SmartLookup.sql
+++ SmartLookup.sql
@@ -1,7 +1,7 @@
--- SmartLookup View (the source of the Actual Timeline view a user sees) 
+-- SmartLookup View  
 -- in easy to view format.
 -- BLOBs and information stored in them needs manual extraction.
--- No JSON1 extension required for this query to run.
+-- JSON1 extension is NOT required for this query to run.
 --
 -- Costas Katsavounidis (kacos2000 [at] gmail.com)
 -- May 2018
