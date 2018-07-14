22df938e46e32aa74103d18cb068ef86f8f80d2d
Costas K <11378310+kacos2000@users.noreply.github.com>
2018-07-15 01:17:11 +0300

    Add files via upload

---
+++ FindSQLite3.ps1
@@ -0,0 +1,8 @@
+﻿# Search the system path (Env:Path) directories for the existence for SQLite3.exe 
+
+$path = (Get-ChildItem Env:Path).value.split("{;}");$i=0
+foreach ($line in $path)
+    {
+        $i++; write-progress -Activity "Searching $line" -Status 'Progress->' -PercentComplete ($i/$path.count*100)
+        Get-Childitem –Path $line -Recurse -Include 'sqlite3.exe' -ErrorAction SilentlyContinue
+    }
