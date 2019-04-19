49b0d563aa80774977139db3763972ef9276d419
Costas K <11378310+kacos2000@users.noreply.github.com>
2019-04-19 21:32:38 +0300

    Add files via upload

--- clipboard1903.sql
+++ clipboard1903.sql
@@ -29,16 +29,12 @@ Select
 			'{'||'F38BF404-1D43-42F2-9305-67DE0B28FC23'||'}', '*Windows'),
 			'{'||'D65231B0-B2F1-4857-A4CE-A8E7C6EA7D27'||'}', '*System32') 
 	end as 'Application',
-
-
   case when json_extract(ActivityOperation.ClipboardPayload, '$[0].formatName') = 'Text' then 
   json_extract(ActivityOperation.ClipboardPayload, '$[0].content') else ' ' end as 'Clip Text(Base64)',
   json_extract(ActivityOperation.ClipboardPayload, '$[0].formatName') as 'Format',
   json_extract(ActivityOperation.Payload, '$.1[0].content') as 'Payload Content', 
   json_extract(ActivityOperation.Payload, '$.1[0].formatName')  as 'Type',
-  
- 
-	case ActivityOperation.ActivityType 
+  case ActivityOperation.ActivityType 
 		when 5 then 'Open App/File/Page' when 6 then 'App In Use/Focus' 
 		when 10 then 'Clipboard' when 16 then 'Copy/Paste'
 		else ActivityOperation.ActivityType end as 'Activity_type',
@@ -46,15 +42,22 @@ Select
   ActivityOperation.GroupAppActivityId as 'GroupAppActivityId',
   ActivityOperation.GroupItems as 'GroupItems',
   json_extract(ActivityOperation.Payload, '$.clipboardDataId') as 'clipboardDataId',
-  hex(ActivityOperation.ParentActivityId) as 'ParentActivityId',
-  
+ '{' || substr(hex(Activity_PackageId.ActivityId), 1, 8) || '-' ||
+		substr(hex(Activity_PackageId.ActivityId), 9, 4) || '-' ||
+		substr(hex(Activity_PackageId.ActivityId), 13, 4) || '-' ||
+		substr(hex(Activity_PackageId.ActivityId), 17, 4) || '-' ||
+		substr(hex(Activity_PackageId.ActivityId), 21, 12) || '}' as 'ActivityId',
+  case when hex(ActivityOperation.ParentActivityId) = '00000000000000000000000000000000'
+	then '' else  
+	 '{' || substr(hex(ActivityOperation.ParentActivityId), 1, 8) || '-' || 
+			substr(hex(ActivityOperation.ParentActivityId), 9, 4) || '-' || 
+			substr(hex(ActivityOperation.ParentActivityId), 13, 4) || '-' || 
+			substr(hex(ActivityOperation.ParentActivityId), 17, 4) || '-' || 
+			substr(hex(ActivityOperation.ParentActivityId), 21, 12) || '}' end as 'ParentActivityId',
   ActivityOperation.DdsDeviceId as 'DdsDeviceId',
   ActivityOperation.PlatformDeviceId as 'Device ID', 
    cast((ActivityOperation.ExpirationTime - ActivityOperation.LastModifiedTime) as integer) / '86400' as 'Expires In days',
-
-   
-
-	case 
+  case 
 		when ActivityOperation.OriginalLastModifiedOnClient > 0 
 			then datetime(ActivityOperation.OriginalLastModifiedOnClient, 'unixepoch', 'localtime') 
 			else '  -  ' 
@@ -69,17 +72,10 @@ Select
 			then datetime(ActivityOperation.CreatedInCloud, 'unixepoch', 'localtime') 
 			else "-" 
 	end as 'CreatedInCloud',
-
    datetime(Activity_PackageId.ExpirationTime, 'unixepoch', 'localtime') as 'Expiration on PackageID',
    datetime(ActivityOperation.ExpirationTime, 'unixepoch', 'localtime') as 'Expiration',  
    '' as 'IsLocalOnly',
 	   ActivityOperation.PackageIdHash as 'PackageIdHash',
-		 '{' || substr(hex(Activity_PackageId.ActivityId), 1, 8) || '-' ||
-				substr(hex(Activity_PackageId.ActivityId), 9, 4) || '-' ||
-				substr(hex(Activity_PackageId.ActivityId), 13, 4) || '-' ||
-				substr(hex(Activity_PackageId.ActivityId), 17, 4) || '-' ||
-				substr(hex(Activity_PackageId.ActivityId), 21, 12) || '}' as 'ID',
-				
 	 ActivityOperation.Payload as 'Payload',	
 	 ActivityOperation.ClipboardPayload as 'ClipboardPayload'
  
@@ -94,7 +90,7 @@ union  -- Join Activity & ActivityOperation Queries to get results from both Tab
 Select
     Activity.ETag as 'Etag',
 	datetime(Activity.StartTime, 'unixepoch', 'localtime') as 'StartTime',
-   datetime(Activity.LastModifiedTime, 'unixepoch', 'localtime') as 'LastModified',
+    datetime(Activity.LastModifiedTime, 'unixepoch', 'localtime') as 'LastModified',
 	case 
 		when json_extract(Activity.AppId, '$[0].application') = '308046B0AF4A39CB' 
 			then 'Mozilla Firefox-64bit'
@@ -121,36 +117,38 @@ Select
 			'{'||'1AC14E77-02E7-4E5D-B744-2EB1AE5198B7'||'}', '*System' ),
 			'{'||'F38BF404-1D43-42F2-9305-67DE0B28FC23'||'}', '*Windows'),
 			'{'||'D65231B0-B2F1-4857-A4CE-A8E7C6EA7D27'||'}', '*System32') 
-	end as 'Application',
-
-
-  case when json_extract(Activity.ClipboardPayload, '$[0].formatName') = 'Text' then 
-  json_extract(Activity.ClipboardPayload, '$[0].content') else ' ' end as 'Clip Text(Base64)',
-  json_extract(Activity.ClipboardPayload, '$[0].formatName') as 'Format',
-  json_extract(Activity.Payload, '$.1[0].content') as 'Payload Content', 
-  json_extract(Activity.Payload, '$.1[0].formatName')  as 'Type',
-  
- 
-	case Activity.ActivityType 
+		end as 'Application',
+    case when json_extract(Activity.ClipboardPayload, '$[0].formatName') = 'Text' then 
+	json_extract(Activity.ClipboardPayload, '$[0].content') else ' ' end as 'Clip Text(Base64)',
+	json_extract(Activity.ClipboardPayload, '$[0].formatName') as 'Format',
+	json_extract(Activity.Payload, '$.1[0].content') as 'Payload Content', 
+	json_extract(Activity.Payload, '$.1[0].formatName')  as 'Type',
+  	case Activity.ActivityType 
 		when 5 then 'Open App/File/Page' when 6 then 'App In Use/Focus' 
 		when 10 then 'Clipboard' when 16 then 'Copy/Paste'
 		else Activity.ActivityType end as 'Activity_type',
-  Activity."Group" as 'Group',
-  Activity.GroupAppActivityId as 'GroupAppActivityId',
-  Activity.GroupItems as 'GroupItems',
-  json_extract(Activity.Payload, '$.clipboardDataId') as 'clipboardDataId',
-  hex(Activity.ParentActivityId) as 'ParentActivityId',
-  
-  Activity.DdsDeviceId as 'DdsDeviceId',
-  Activity.PlatformDeviceId as 'Device ID', 
-   cast((Activity.ExpirationTime - Activity.LastModifiedTime) as integer) / '86400' as 'Expires In days',
-
-   
-
-	case 
-		when Activity.OriginalLastModifiedOnClient > 0 
-			then datetime(Activity.OriginalLastModifiedOnClient, 'unixepoch', 'localtime') 
-			else '  -  ' 
+    Activity."Group" as 'Group',
+    Activity.GroupAppActivityId as 'GroupAppActivityId',
+    Activity.GroupItems as 'GroupItems',
+    '{' || substr(hex(Activity_PackageId.ActivityId), 1, 8) || '-' ||
+		substr(hex(Activity_PackageId.ActivityId), 9, 4) || '-' ||
+		substr(hex(Activity_PackageId.ActivityId), 13, 4) || '-' ||
+		substr(hex(Activity_PackageId.ActivityId), 17, 4) || '-' ||
+		substr(hex(Activity_PackageId.ActivityId), 21, 12) || '}' as 'ActivityId',
+    case when hex(Activity.ParentActivityId) = '00000000000000000000000000000000'
+	then '' else  
+	 '{' || substr(hex(Activity.ParentActivityId), 1, 8) || '-' || 
+			substr(hex(Activity.ParentActivityId), 9, 4) || '-' || 
+			substr(hex(Activity.ParentActivityId), 13, 4) || '-' || 
+			substr(hex(Activity.ParentActivityId), 17, 4) || '-' || 
+			substr(hex(Activity.ParentActivityId), 21, 12) || '}' end as 'ParentActivityId',
+    json_extract(Activity.Payload, '$.clipboardDataId') as 'clipboardDataId',
+    Activity.DdsDeviceId as 'DdsDeviceId',
+    Activity.PlatformDeviceId as 'Device ID', 
+    cast((Activity.ExpirationTime - Activity.LastModifiedTime) as integer) / '86400' as 'Expires In days',
+    case when Activity.OriginalLastModifiedOnClient > 0 
+	     then datetime(Activity.OriginalLastModifiedOnClient, 'unixepoch', 'localtime') 
+		 else '  -  ' 
 	end as 'LastModifiedOnClient',
 	case 
 		when Activity.EndTime > 0 
@@ -162,19 +160,12 @@ Select
 			then datetime(Activity.CreatedInCloud, 'unixepoch', 'localtime') 
 			else "-" 
 	end as 'CreatedInCloud',
-
-   datetime(Activity_PackageId.ExpirationTime, 'unixepoch', 'localtime') as 'Expiration on PackageID',
-   datetime(Activity.ExpirationTime, 'unixepoch', 'localtime') as 'Expiration',  
+    datetime(Activity_PackageId.ExpirationTime, 'unixepoch', 'localtime') as 'Expiration on PackageID',
+    datetime(Activity.ExpirationTime, 'unixepoch', 'localtime') as 'Expiration',  
     case Activity.IsLocalOnly when 0 then 'No' when 1 then 'Yes' else Activity.IsLocalOnly end as 'IsLocalOnly',
-	   Activity.PackageIdHash as 'PackageIdHash',
-		 '{' || substr(hex(Activity_PackageId.ActivityId), 1, 8) || '-' ||
-				substr(hex(Activity_PackageId.ActivityId), 9, 4) || '-' ||
-				substr(hex(Activity_PackageId.ActivityId), 13, 4) || '-' ||
-				substr(hex(Activity_PackageId.ActivityId), 17, 4) || '-' ||
-				substr(hex(Activity_PackageId.ActivityId), 21, 12) || '}' as 'ID',
-				
-	 Activity.Payload as 'Payload',	
-	 Activity.ClipboardPayload as 'ClipboardPayload'
+	Activity.PackageIdHash as 'PackageIdHash',
+	Activity.Payload as 'Payload',	
+	Activity.ClipboardPayload as 'ClipboardPayload'
       
 
  
@@ -183,6 +174,6 @@ join Activity on Activity_PackageId.ActivityId = Activity.Id
 where 	Activity_PackageId.Platform = json_extract(Activity.AppId, '$[0].platform')
 	and Activity_PackageId.ActivityId = Activity.Id and Activity.ActivityType in (10,16)
 
-	order by Etag desc;
+order by Etag desc;
 
 
